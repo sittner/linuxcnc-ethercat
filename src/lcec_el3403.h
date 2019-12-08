fbb888e778e455355b61324f515cdb56a1fe8621
Scott Laird <laird@google.com>
2019-12-08 15:37:27 -0800

    Replace my EL3403 module with A.Chimeno's from the LinuxCNC forum.
    His appears to actually work, unlike mine which doesn't seem to
    initialize the module correctly.  I applied a couple minor spelling
    changes.

--- src/lcec_el3403.h
+++ src/lcec_el3403.h
@@ -1,5 +1,5 @@
 //
-//    Copyright (C) 2016 Sascha Ittner <sascha.ittner@modusoft.de>
+//    Copyright (C) 2011 Sascha Ittner <sascha.ittner@modusoft.de>
 //
 //    This program is free software; you can redistribute it and/or modify
 //    it under the terms of the GNU General Public License as published by
@@ -15,18 +15,21 @@
 //    along with this program; if not, write to the Free Software
 //    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 //
+//    Edit A.Chimeno 14/06/19 Rev 2.0
+
 #ifndef _LCEC_EL3403_H_
 #define _LCEC_EL3403_H_
 
 #include "lcec.h"
 
 #define LCEC_EL3403_VID LCEC_BECKHOFF_VID
+
 #define LCEC_EL3403_PID 0x0d4b3052
 
 #define LCEC_EL3403_CHANS 3
-#define LCEC_EL3403_PDOS  (LCEC_EL3403_CHANS * 5)
+                         
+#define LCEC_EL3403_PDOS 29
 
 int lcec_el3403_init(int comp_id, struct lcec_slave *slave, ec_pdo_entry_reg_t *pdo_entry_regs);
 
 #endif
-
