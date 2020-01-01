3aa23ab2466d72e2eda1b74b4d5d08641d1d1683
Scott Laird <laird@google.com>
2020-01-01 15:52:49 -0800

    Fix phantom sync error on 0x6000:0e.
    Delete sync error code.
    
    Seems to work now.

--- src/lcec_em37xx.c
+++ src/lcec_em37xx.c
@@ -23,7 +23,6 @@ typedef struct {
   hal_bit_t *overrange;
   hal_bit_t *underrange;
   hal_bit_t *error;
-  hal_bit_t *sync_err;
   hal_s32_t *raw_val;
   hal_float_t *scale;
   hal_float_t *bias;
@@ -34,8 +33,6 @@ typedef struct {
   unsigned int udr_pdo_bp;
   unsigned int error_pdo_os;
   unsigned int error_pdo_bp;
-  unsigned int sync_err_pdo_os;
-  unsigned int sync_err_pdo_bp;
   unsigned int val_pdo_os;
 } lcec_em37xx_chan_t;
 
@@ -47,7 +44,6 @@ static const lcec_pindesc_t slave_pins[] = {
   { HAL_BIT, HAL_OUT, offsetof(lcec_em37xx_chan_t, overrange), "%s.%s.%s.press-%d-overrange" },
   { HAL_BIT, HAL_OUT, offsetof(lcec_em37xx_chan_t, underrange), "%s.%s.%s.press-%d-underrange" },
   { HAL_BIT, HAL_OUT, offsetof(lcec_em37xx_chan_t, error), "%s.%s.%s.press-%d-error" },
-  { HAL_BIT, HAL_OUT, offsetof(lcec_em37xx_chan_t, sync_err), "%s.%s.%s.press-%d-sync-err" },
   { HAL_S32, HAL_OUT, offsetof(lcec_em37xx_chan_t, raw_val), "%s.%s.%s.press-%d-raw" },
   { HAL_FLOAT, HAL_OUT, offsetof(lcec_em37xx_chan_t, val), "%s.%s.%s.press-%d-pressure" },
   { HAL_FLOAT, HAL_IO, offsetof(lcec_em37xx_chan_t, scale), "%s.%s.%s.press-%d-scale" },
@@ -81,8 +77,8 @@ static ec_pdo_entry_info_t lcec_em37xx_channel2[] = {
 };
 
 static ec_pdo_info_t lcec_em37xx_pdos_in[] = {
-    {0x1A00, 11, lcec_em37xx_channel1},
-    {0x1A01, 11, lcec_em37xx_channel2},
+    {0x1A00, 9, lcec_em37xx_channel1},
+    {0x1A01, 9, lcec_em37xx_channel2},
 };
 
 static ec_sync_info_t lcec_em37xx_syncs[] = {
@@ -124,7 +120,6 @@ int lcec_em37xx_init(int comp_id, struct lcec_slave *slave, ec_pdo_entry_reg_t *
     LCEC_PDO_INIT(pdo_entry_regs, slave->index, slave->vid, slave->pid, 0x6000 + (i << 4), 0x01, &chan->ovr_pdo_os, &chan->ovr_pdo_bp);
     LCEC_PDO_INIT(pdo_entry_regs, slave->index, slave->vid, slave->pid, 0x6000 + (i << 4), 0x02, &chan->udr_pdo_os, &chan->udr_pdo_bp);
     LCEC_PDO_INIT(pdo_entry_regs, slave->index, slave->vid, slave->pid, 0x6000 + (i << 4), 0x07, &chan->error_pdo_os, &chan->error_pdo_bp);
-    LCEC_PDO_INIT(pdo_entry_regs, slave->index, slave->vid, slave->pid, 0x6000 + (i << 4), 0x0e, &chan->sync_err_pdo_os, &chan->sync_err_pdo_bp);
     LCEC_PDO_INIT(pdo_entry_regs, slave->index, slave->vid, slave->pid, 0x6000 + (i << 4), 0x11, &chan->val_pdo_os, NULL);
 
     // export pins
@@ -160,7 +155,6 @@ void lcec_em37xx_read(struct lcec_slave *slave, long period) {
     *(chan->overrange) = EC_READ_BIT(&pd[chan->ovr_pdo_os], chan->ovr_pdo_bp);
     *(chan->underrange) = EC_READ_BIT(&pd[chan->udr_pdo_os], chan->udr_pdo_bp);
     *(chan->error) = EC_READ_BIT(&pd[chan->error_pdo_os], chan->error_pdo_bp);
-    *(chan->sync_err) = EC_READ_BIT(&pd[chan->sync_err_pdo_os], chan->sync_err_pdo_bp);
 
     // update value
     value = EC_READ_S16(&pd[chan->val_pdo_os]);
