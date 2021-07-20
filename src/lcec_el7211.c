4addb6b70a215ae99093e6ab9d7c1cc1de61c989
Sascha Ittner <sascha.ittner@modusoft.de>
2021-07-20 16:53:33 +0200

    route digital input to HAL for EL7201-9014

--- src/lcec_el7211.c
+++ src/lcec_el7211.c
@@ -38,6 +38,12 @@ typedef struct {
   hal_bit_t *status_warning;
   hal_bit_t *status_limit_active;
 
+  hal_bit_t *input_0;
+  hal_bit_t *input_0_not;
+  hal_bit_t *input_1;
+  hal_bit_t *input_1_not;
+  hal_bit_t *input_sto;
+
   hal_bit_t *at_speed;
 
   hal_float_t *vel_cmd;
@@ -55,7 +61,7 @@ typedef struct {
   hal_float_t min_vel;
   hal_float_t max_vel;
   hal_float_t max_accel;
-  hal_float_t  at_speed_window;
+  hal_float_t at_speed_window;
 
   lcec_class_enc_data_t enc;
 
@@ -64,6 +70,7 @@ typedef struct {
   unsigned int vel_fb_pdo_os;
   unsigned int ctrl_pdo_os;
   unsigned int vel_cmd_pdo_os;
+  unsigned int info1_pdo_os;
 
   double vel_scale;
   double vel_rcpt;
@@ -98,6 +105,15 @@ static const lcec_pindesc_t slave_pins[] = {
   { HAL_TYPE_UNSPECIFIED, HAL_DIR_UNSPECIFIED, -1, NULL }
 };
 
+static const lcec_pindesc_t slave_pins_el7201_9014[] = {
+  { HAL_BIT, HAL_OUT, offsetof(lcec_el7211_data_t, input_0), "%s.%s.%s.input-0" },
+  { HAL_BIT, HAL_OUT, offsetof(lcec_el7211_data_t, input_0_not), "%s.%s.%s.input-0-not" },
+  { HAL_BIT, HAL_OUT, offsetof(lcec_el7211_data_t, input_1), "%s.%s.%s.input-1" },
+  { HAL_BIT, HAL_OUT, offsetof(lcec_el7211_data_t, input_1_not), "%s.%s.%s.input-1-not" },
+  { HAL_BIT, HAL_OUT, offsetof(lcec_el7211_data_t, input_sto), "%s.%s.%s.input-sto" },
+  { HAL_TYPE_UNSPECIFIED, HAL_DIR_UNSPECIFIED, -1, NULL }
+};
+
 static const lcec_pindesc_t slave_params[] = {
   { HAL_FLOAT, HAL_RW, offsetof(lcec_el7211_data_t, scale), "%s.%s.%s.scale" },
   { HAL_U32, HAL_RO, offsetof(lcec_el7211_data_t, vel_resolution), "%s.%s.%s.vel-resolution" },
@@ -115,6 +131,10 @@ static ec_pdo_entry_info_t lcec_el7211_in_pos[] = {
 
 static ec_pdo_entry_info_t lcec_el7211_in_status[] = {
    {0x6010, 0x01, 16}  // status word
+};
+
+static ec_pdo_entry_info_t lcec_el7211_in_info1[] = {
+   {0x6010, 0x12, 16}  // info 1
 };
 
 static ec_pdo_entry_info_t lcec_el7211_in_vel[] = {
@@ -135,6 +155,13 @@ static ec_pdo_info_t lcec_el7211_pdos_in[] = {
     {0x1A02, 1, lcec_el7211_in_vel},
 };
 
+static ec_pdo_info_t lcec_el7201_9014_pdos_in[] = {
+    {0x1A00, 1, lcec_el7211_in_pos},
+    {0x1A01, 1, lcec_el7211_in_status},
+    {0x1A02, 1, lcec_el7211_in_vel},
+    {0x1A04, 1, lcec_el7211_in_info1},
+};
+
 static ec_pdo_info_t lcec_el7211_pdos_out[] = {
     {0x1600, 1, lcec_el7211_out_ctrl},
     {0x1601, 1, lcec_el7211_out_cmd},
@@ -148,30 +175,39 @@ static ec_sync_info_t lcec_el7211_syncs[] = {
     {0xff}
 };
 
+static ec_sync_info_t lcec_el7201_9014_syncs[] = {
+    {0, EC_DIR_OUTPUT, 0, NULL},
+    {1, EC_DIR_INPUT,  0, NULL},
+    {2, EC_DIR_OUTPUT, 2, lcec_el7211_pdos_out},
+    {3, EC_DIR_INPUT,  4, lcec_el7201_9014_pdos_in},
+    {0xff}
+};
 
+lcec_el7211_data_t *lcec_el7211_alloc_hal(lcec_master_t *master, struct lcec_slave *slave);
+int lcec_el7211_export_pins(lcec_master_t *master, struct lcec_slave *slave, lcec_el7211_data_t *hal_data);
 void lcec_el7211_check_scales(lcec_el7211_data_t *hal_data);
 void lcec_el7211_read(struct lcec_slave *slave, long period);
+void lcec_el7201_9014_read(struct lcec_slave *slave, long period);
 void lcec_el7211_write(struct lcec_slave *slave, long period);
 
-int lcec_el7211_init(int comp_id, struct lcec_slave *slave, ec_pdo_entry_reg_t *pdo_entry_regs) {
-  lcec_master_t *master = slave->master;
+lcec_el7211_data_t *lcec_el7211_alloc_hal(lcec_master_t *master, struct lcec_slave *slave) {
   lcec_el7211_data_t *hal_data;
-  int err;
-  uint8_t sdo_buf[4];
-  uint32_t sdo_vel_resolution;
-  uint32_t sdo_pos_resolution;
-
-  // initialize callbacks
-  slave->proc_read = lcec_el7211_read;
-  slave->proc_write = lcec_el7211_write;
 
   // alloc hal memory
   if ((hal_data = hal_malloc(sizeof(lcec_el7211_data_t))) == NULL) {
     rtapi_print_msg(RTAPI_MSG_ERR, LCEC_MSG_PFX "hal_malloc() for slave %s.%s failed\n", master->name, slave->name);
-    return -EIO;
+    return NULL;
   }
   memset(hal_data, 0, sizeof(lcec_el7211_data_t));
-  slave->hal_data = hal_data;
+
+  return hal_data;
+}
+
+int lcec_el7211_export_pins(lcec_master_t *master, struct lcec_slave *slave, lcec_el7211_data_t *hal_data) {
+  int err;
+  uint8_t sdo_buf[4];
+  uint32_t sdo_vel_resolution;
+  uint32_t sdo_pos_resolution;
 
   // read sdos
   if (lcec_read_sdo(slave, 0x9010, 0x14, sdo_buf, 4)) {
@@ -182,16 +218,6 @@ int lcec_el7211_init(int comp_id, struct lcec_slave *slave, ec_pdo_entry_reg_t *
     return -EIO;
   }
   sdo_pos_resolution = EC_READ_U32(sdo_buf);
-
-  // initialize sync info
-  slave->sync_info = lcec_el7211_syncs;
-
-  // initialize POD entries
-  LCEC_PDO_INIT(pdo_entry_regs, slave->index, slave->vid, slave->pid, 0x6000, 0x11, &hal_data->pos_fb_pdo_os, NULL);
-  LCEC_PDO_INIT(pdo_entry_regs, slave->index, slave->vid, slave->pid, 0x6010, 0x01, &hal_data->status_pdo_os, NULL);
-  LCEC_PDO_INIT(pdo_entry_regs, slave->index, slave->vid, slave->pid, 0x6010, 0x07, &hal_data->vel_fb_pdo_os, NULL);
-  LCEC_PDO_INIT(pdo_entry_regs, slave->index, slave->vid, slave->pid, 0x7010, 0x01, &hal_data->ctrl_pdo_os, NULL);
-  LCEC_PDO_INIT(pdo_entry_regs, slave->index, slave->vid, slave->pid, 0x7010, 0x06, &hal_data->vel_cmd_pdo_os, NULL);
 
   // export pins
   if ((err = lcec_pin_newf_list(hal_data, slave_pins, LCEC_MODULE_NAME, master->name, slave->name)) != 0) {
@@ -229,6 +255,87 @@ int lcec_el7211_init(int comp_id, struct lcec_slave *slave, ec_pdo_entry_reg_t *
   hal_data->min_vel = -1e20;
   hal_data->max_vel = 1e20;
   hal_data->max_accel = 1e20;
+
+  return 0;
+}
+
+int lcec_el7211_init(int comp_id, struct lcec_slave *slave, ec_pdo_entry_reg_t *pdo_entry_regs) {
+  lcec_master_t *master = slave->master;
+  lcec_el7211_data_t *hal_data;
+  int err;
+
+  if ((hal_data = lcec_el7211_alloc_hal(master, slave)) == NULL) {
+    return -EIO;
+  }
+  slave->hal_data = hal_data;
+
+  // initialize callbacks
+  slave->proc_read = lcec_el7211_read;
+  slave->proc_write = lcec_el7211_write;
+
+  // initialize sync info
+  slave->sync_info = lcec_el7211_syncs;
+
+  // initialize POD entries
+  LCEC_PDO_INIT(pdo_entry_regs, slave->index, slave->vid, slave->pid, 0x6000, 0x11, &hal_data->pos_fb_pdo_os, NULL);
+  LCEC_PDO_INIT(pdo_entry_regs, slave->index, slave->vid, slave->pid, 0x6010, 0x01, &hal_data->status_pdo_os, NULL);
+  LCEC_PDO_INIT(pdo_entry_regs, slave->index, slave->vid, slave->pid, 0x6010, 0x07, &hal_data->vel_fb_pdo_os, NULL);
+  LCEC_PDO_INIT(pdo_entry_regs, slave->index, slave->vid, slave->pid, 0x7010, 0x01, &hal_data->ctrl_pdo_os, NULL);
+  LCEC_PDO_INIT(pdo_entry_regs, slave->index, slave->vid, slave->pid, 0x7010, 0x06, &hal_data->vel_cmd_pdo_os, NULL);
+
+  // export pins
+  if ((err = lcec_el7211_export_pins(master, slave, hal_data)) != 0) {
+    return err;
+  }
+
+  return 0;
+}
+
+int lcec_el7201_9014_init(int comp_id, struct lcec_slave *slave, ec_pdo_entry_reg_t *pdo_entry_regs) {
+  lcec_master_t *master = slave->master;
+  lcec_el7211_data_t *hal_data;
+  int err;
+
+  if ((hal_data = lcec_el7211_alloc_hal(master, slave)) == NULL) {
+    return -EIO;
+  }
+  slave->hal_data = hal_data;
+
+  // set info1 to inputs
+  if (ecrt_slave_config_sdo8(slave->config, 0x8010, 0x39, 10) != 0) {
+    rtapi_print_msg (RTAPI_MSG_ERR, LCEC_MSG_PFX "fail to configure slave %s.%s sdo info1 select\n", master->name, slave->name);
+    return -1;
+  }
+
+  // initialize callbacks
+  slave->proc_read = lcec_el7201_9014_read;
+  slave->proc_write = lcec_el7211_write;
+
+  // initialize sync info
+  slave->sync_info = lcec_el7201_9014_syncs;
+
+  // initialize POD entries
+  LCEC_PDO_INIT(pdo_entry_regs, slave->index, slave->vid, slave->pid, 0x6000, 0x11, &hal_data->pos_fb_pdo_os, NULL);
+  LCEC_PDO_INIT(pdo_entry_regs, slave->index, slave->vid, slave->pid, 0x6010, 0x01, &hal_data->status_pdo_os, NULL);
+  LCEC_PDO_INIT(pdo_entry_regs, slave->index, slave->vid, slave->pid, 0x6010, 0x07, &hal_data->vel_fb_pdo_os, NULL);
+  LCEC_PDO_INIT(pdo_entry_regs, slave->index, slave->vid, slave->pid, 0x7010, 0x01, &hal_data->ctrl_pdo_os, NULL);
+  LCEC_PDO_INIT(pdo_entry_regs, slave->index, slave->vid, slave->pid, 0x7010, 0x06, &hal_data->vel_cmd_pdo_os, NULL);
+  LCEC_PDO_INIT(pdo_entry_regs, slave->index, slave->vid, slave->pid, 0x6010, 0x12, &hal_data->info1_pdo_os, NULL);
+
+  // export pins
+  if ((err = lcec_el7211_export_pins(master, slave, hal_data)) != 0) {
+    return err;
+  }
+
+  // export extra pins
+  if ((err = lcec_pin_newf_list(hal_data, slave_pins_el7201_9014, LCEC_MODULE_NAME, master->name, slave->name)) != 0) {
+    return err;
+  }
+
+  // initialize extra variables
+  *(hal_data->input_0) = 0;
+  *(hal_data->input_1) = 0;
+  *(hal_data->input_sto) = 0;
 
   return 0;
 }
@@ -314,6 +421,23 @@ void lcec_el7211_read(struct lcec_slave *slave, long period) {
   // update position feedback
   pos_cnt = EC_READ_U32(&pd[hal_data->pos_fb_pdo_os]);
   class_enc_update(&hal_data->enc, hal_data->pos_resolution, hal_data->scale_rcpt, pos_cnt, 0, 0);
+}
+
+void lcec_el7201_9014_read(struct lcec_slave *slave, long period) {
+  lcec_master_t *master = slave->master;
+  lcec_el7211_data_t *hal_data = (lcec_el7211_data_t *) slave->hal_data;
+  uint8_t *pd = master->process_data;
+  uint16_t info1;
+
+  lcec_el7211_read(slave, period);
+
+  // read info1
+  info1 = EC_READ_U16(&pd[hal_data->info1_pdo_os]);
+  *(hal_data->input_0) = (info1 >> 0) & 0x01;
+  *(hal_data->input_0_not) = !*(hal_data->input_0);
+  *(hal_data->input_1) = (info1 >> 1) & 0x01;
+  *(hal_data->input_1_not) = !*(hal_data->input_1);
+  *(hal_data->input_sto) = (info1 >> 8) & 0x01;
 }
 
 static inline double clamp(double v, double sub, double sup) {
