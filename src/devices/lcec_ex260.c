8c2e66658a061a3cef9cf7fa5518e6be08b0cd50
Scott Laird <scott@sigkill.org>
2023-12-31 12:24:59 -0800

    build: clang-format ex260, and fix minor bug

--- src/devices/lcec_ex260.c
+++ src/devices/lcec_ex260.c
@@ -6,39 +6,39 @@
 
 static int lcec_ex260_init(int comp_id, struct lcec_slave *slave, ec_pdo_entry_reg_t *pdo_entry_regs);
 
-static lcec_typelist_t types[]={
-  { "EX260-SEC1", LCEC_EX260_VID, LCEC_EX260_SEC1_PID, LCEC_EX260_SEC1_PDOS, lcec_ex260_init},
-  { "EX260-SEC2", LCEC_EX260_VID, LCEC_EX260_SEC2_PID, LCEC_EX260_SEC2_PDOS, lcec_ex260_init},
-  { "EX260-SEC3", LCEC_EX260_VID, LCEC_EX260_SEC3_PID, LCEC_EX260_SEC3_PDOS, lcec_ex260_init},
-  { "EX260-SEC4", LCEC_EX260_VID, LCEC_EX260_SEC4_PID, LCEC_EX260_SEC4_PDOS, lcec_ex260_init},
-  { NULL },
+static lcec_typelist_t types[] = {
+    {"EX260-SEC1", LCEC_EX260_VID, LCEC_EX260_SEC1_PID, LCEC_EX260_SEC1_PDOS, 0, NULL, lcec_ex260_init},
+    {"EX260-SEC2", LCEC_EX260_VID, LCEC_EX260_SEC2_PID, LCEC_EX260_SEC2_PDOS, 0, NULL, lcec_ex260_init},
+    {"EX260-SEC3", LCEC_EX260_VID, LCEC_EX260_SEC3_PID, LCEC_EX260_SEC3_PDOS, 0, NULL, lcec_ex260_init},
+    {"EX260-SEC4", LCEC_EX260_VID, LCEC_EX260_SEC4_PID, LCEC_EX260_SEC4_PDOS, 0, NULL, lcec_ex260_init},
+    {NULL},
 };
 
 ADD_TYPES(types);
 
 typedef struct {
-    hal_bit_t *sol_1a;
-    hal_bit_t *sol_1b;
-    hal_bit_t *sol_2a;
-    hal_bit_t *sol_2b;
-    hal_bit_t *sol_3a;
-    hal_bit_t *sol_3b;
-    hal_bit_t *sol_4a;
-    hal_bit_t *sol_4b;
-    unsigned int pdo_os;
-    unsigned int pdo_bp;
+  hal_bit_t *sol_1a;
+  hal_bit_t *sol_1b;
+  hal_bit_t *sol_2a;
+  hal_bit_t *sol_2b;
+  hal_bit_t *sol_3a;
+  hal_bit_t *sol_3b;
+  hal_bit_t *sol_4a;
+  hal_bit_t *sol_4b;
+  unsigned int pdo_os;
+  unsigned int pdo_bp;
 } lcec_ex260_pin_t;
 
 static const lcec_pindesc_t slave_pins[] = {
-        { HAL_BIT, HAL_IN, offsetof(lcec_ex260_pin_t, sol_1a), "%s.%s.%s.sol-%d-1a" },
-        { HAL_BIT, HAL_IN, offsetof(lcec_ex260_pin_t, sol_1b), "%s.%s.%s.sol-%d-1b" },
-        { HAL_BIT, HAL_IN, offsetof(lcec_ex260_pin_t, sol_2a), "%s.%s.%s.sol-%d-2a" },
-        { HAL_BIT, HAL_IN, offsetof(lcec_ex260_pin_t, sol_2b), "%s.%s.%s.sol-%d-2b" },
-        { HAL_BIT, HAL_IN, offsetof(lcec_ex260_pin_t, sol_3a), "%s.%s.%s.sol-%d-3a" },
-        { HAL_BIT, HAL_IN, offsetof(lcec_ex260_pin_t, sol_3b), "%s.%s.%s.sol-%d-3b" },
-        { HAL_BIT, HAL_IN, offsetof(lcec_ex260_pin_t, sol_4a), "%s.%s.%s.sol-%d-4a" },
-        { HAL_BIT, HAL_IN, offsetof(lcec_ex260_pin_t, sol_4b), "%s.%s.%s.sol-%d-4b" },
-        { HAL_TYPE_UNSPECIFIED, HAL_DIR_UNSPECIFIED, -1, NULL }
+    {HAL_BIT, HAL_IN, offsetof(lcec_ex260_pin_t, sol_1a), "%s.%s.%s.sol-%d-1a"},
+    {HAL_BIT, HAL_IN, offsetof(lcec_ex260_pin_t, sol_1b), "%s.%s.%s.sol-%d-1b"},
+    {HAL_BIT, HAL_IN, offsetof(lcec_ex260_pin_t, sol_2a), "%s.%s.%s.sol-%d-2a"},
+    {HAL_BIT, HAL_IN, offsetof(lcec_ex260_pin_t, sol_2b), "%s.%s.%s.sol-%d-2b"},
+    {HAL_BIT, HAL_IN, offsetof(lcec_ex260_pin_t, sol_3a), "%s.%s.%s.sol-%d-3a"},
+    {HAL_BIT, HAL_IN, offsetof(lcec_ex260_pin_t, sol_3b), "%s.%s.%s.sol-%d-3b"},
+    {HAL_BIT, HAL_IN, offsetof(lcec_ex260_pin_t, sol_4a), "%s.%s.%s.sol-%d-4a"},
+    {HAL_BIT, HAL_IN, offsetof(lcec_ex260_pin_t, sol_4b), "%s.%s.%s.sol-%d-4b"},
+    {HAL_TYPE_UNSPECIFIED, HAL_DIR_UNSPECIFIED, -1, NULL}
 };
 
 static void lcec_ex260_write(struct lcec_slave *slave, long period);
@@ -62,14 +62,13 @@ static int lcec_ex260_init(int comp_id, struct lcec_slave *slave, ec_pdo_entry_r
   slave->hal_data = hal_data;
 
   // initialize pins
-  for (i=0, pin=hal_data; i<slave->pdo_entry_count; i++, pin++) {
+  for (i = 0, pin = hal_data; i < slave->pdo_entry_count; i++, pin++) {
     // initialize PDO entry
     LCEC_PDO_INIT(pdo_entry_regs, slave->index, slave->vid, slave->pid, 0x3101, 0x01 + i, &pin->pdo_os, &pin->pdo_bp);
 
     if ((err = lcec_pin_newf_list(pin, slave_pins, LCEC_MODULE_NAME, master->name, slave->name, i)) != 0) {
       return err;
     }
-
   }
 
   return 0;
@@ -77,7 +76,7 @@ static int lcec_ex260_init(int comp_id, struct lcec_slave *slave, ec_pdo_entry_r
 
 static void lcec_ex260_write(struct lcec_slave *slave, long period) {
   lcec_master_t *master = slave->master;
-  lcec_ex260_pin_t *hal_data = (lcec_ex260_pin_t *) slave->hal_data;
+  lcec_ex260_pin_t *hal_data = (lcec_ex260_pin_t *)slave->hal_data;
   uint8_t *pd = master->process_data;
   lcec_ex260_pin_t *pin;
   int i, s;
@@ -88,7 +87,7 @@ static void lcec_ex260_write(struct lcec_slave *slave, long period) {
   }
 
   // set outputs
-  for (i=0, pin=hal_data; i<slave->pdo_entry_count; i++, pin++) {
+  for (i = 0, pin = hal_data; i < slave->pdo_entry_count; i++, pin++) {
     s = *(pin->sol_1a);
     s |= *(pin->sol_1b) << 1;
     s |= *(pin->sol_2a) << 2;
@@ -98,6 +97,5 @@ static void lcec_ex260_write(struct lcec_slave *slave, long period) {
     s |= *(pin->sol_4a) << 6;
     s |= *(pin->sol_4b) << 7;
     EC_WRITE_U8(&pd[pin->pdo_os], s);
-
   }
 }
--- src/devices/lcec_ex260.h
+++ src/devices/lcec_ex260.h
@@ -5,14 +5,14 @@
 #ifndef _LCEC_EX260_H
 #define _LCEC_EX260_H
 
-#include "lcec.h"
-#include "lcec_conf.h"
+#include "../lcec.h"
+//#include "../lcec_conf.h"
 
 /* Master 0, Slave 3, "EX260-SEC3"
-* Vendor ID:       0x00000114
-* Product code:    0x01000003
-* Revision number: 0x00010002
-*/
+ * Vendor ID:       0x00000114
+ * Product code:    0x01000003
+ * Revision number: 0x00010002
+ */
 
 #define LCEC_EX260_VID LCEC_SMC_VID
 #define LCEC_EX260_SEC1_PID 0x01000001
@@ -24,4 +24,4 @@
 #define LCEC_EX260_SEC2_PDOS 4
 #define LCEC_EX260_SEC3_PDOS 2
 #define LCEC_EX260_SEC4_PDOS 2
-#endif //_LCEC_EX260_H
+#endif  //_LCEC_EX260_H
