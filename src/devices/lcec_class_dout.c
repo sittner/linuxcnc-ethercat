ffbb497ef581056e0614e2f1cdbe7da6e08b362d
Scott Laird <scott@sigkill.org>
2024-01-17 20:37:42 -0800

    refactor: create lcec_class_dout and move code from el2xxx

---
+++ src/devices/lcec_class_dout.c
@@ -0,0 +1,121 @@
+//
+//    Copyright (C) 2024 Scott Laird <scott@sigkill.org>
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+//
+
+#include "lcec_class_dout.h"
+
+#include "../lcec.h"
+
+static const lcec_pindesc_t slave_pins[] = {
+    {HAL_BIT, HAL_OUT, offsetof(lcec_class_dout_pin_t, out), "%s.%s.%s.dout-%d"},
+    {HAL_BIT, HAL_OUT, offsetof(lcec_class_dout_pin_t, invert), "%s.%s.%s.dout-%d-invert"},
+    {HAL_TYPE_UNSPECIFIED, HAL_DIR_UNSPECIFIED, -1, NULL},
+};
+
+// lcec_dout_allocate_pins returns a block of memory for holdintg the
+// result of `count` calls to `lcec_dout_register_device()`.  It is the
+// caller's responsibility to verify that the result is not NULL.
+lcec_class_dout_pins_t *lcec_dout_allocate_pins(int count) {
+  lcec_class_dout_pins_t *pins;
+
+  pins = hal_malloc(sizeof(lcec_class_dout_pins_t));
+  if (pins == NULL) {
+    return NULL;
+  }
+  pins->count = count;
+  pins->pins = hal_malloc(sizeof(lcec_class_dout_pin_t *) * count);
+  if (pins->pins == NULL) {
+    return NULL;
+  }
+
+  return pins;
+}
+
+// lcec_dout_register_pin registers a single digital-output channel and publishes it as a LinuxCNC HAL pin.
+//
+// Parameters:
+//
+// - pdo_entry_regs: a pointer to the pdo_entry_regs passed into the device `_init` function.
+// - slave: the slave, from `_init`.
+// - id: the pin ID.  Used for naming.  Should generally start at 0 and increment once per digital out pin.
+// - idx: the PDO index for the digital output.
+// - sindx: the PDO sub-index for the digital output.
+//
+// See lcec_el2xxx.c for an example of use.
+lcec_class_dout_pin_t *lcec_dout_register_pin(
+    ec_pdo_entry_reg_t **pdo_entry_regs, struct lcec_slave *slave, int id, uint16_t idx, uint16_t sidx) {
+  lcec_class_dout_pin_t *data;
+  int err;
+
+  data = hal_malloc(sizeof(lcec_class_dout_pin_t));
+  if (data == NULL) {
+    rtapi_print_msg(RTAPI_MSG_ERR, LCEC_MSG_PFX "hal_malloc() for slave %s.%s pin %d failed\n", slave->master->name, slave->name, id);
+    return NULL;
+  }
+  memset(data, 0, sizeof(lcec_class_dout_pin_t));
+  // data->idx = idx;
+  // data->sidx = sidx;
+
+  LCEC_PDO_INIT((*pdo_entry_regs), slave->index, slave->vid, slave->pid, idx, sidx, &data->pdo_os, &data->pdo_bp);
+  err = lcec_pin_newf_list(data, slave_pins, LCEC_MODULE_NAME, slave->master->name, slave->name, id);
+  if (err != 0) {
+    rtapi_print_msg(RTAPI_MSG_ERR, LCEC_MSG_PFX "lcec_pin_newf_list for slave %s.%s pin %d failed\n", slave->master->name, slave->name, id);
+    return NULL;
+  }
+
+  return data;
+}
+
+// lcec_dout_write writes data to a digital out port.
+//
+// Parameters:
+//
+// - slave: the slave, passed from the per-device `_write`.
+// - data: a lcec_class_dout_pin_t *, as returned by lcec_dout_register_pin.
+//
+// Call this once per pin registered, from inside of your device's
+// write function.
+void lcec_dout_write(struct lcec_slave *slave, lcec_class_dout_pin_t *data) {
+  lcec_master_t *master = slave->master;
+  uint8_t *pd = master->process_data;
+  int s;
+
+  s = *(data->out);
+  if (data->invert) {
+    s = !s;
+  }
+
+  EC_WRITE_BIT(&pd[data->pdo_os], data->pdo_bp, s);
+}
+
+// lcec_dout_write_all writess data to all digital out ports.
+//
+// Parameters:
+//
+// - slave: the slave, passed from the per-device `_write`.
+// - pins: a lcec_class_dout_pins_t *, as returned by lcec_dout_register_pin.
+void lcec_dout_write_all(struct lcec_slave *slave, lcec_class_dout_pins_t *pins) {
+  lcec_master_t *master = slave->master;
+  uint8_t *pd = master->process_data;
+  int s;
+
+  for (int i = 0; i < pins->count; i++) {
+    lcec_class_dout_pin_t *pin = pins->pins[i];
+
+    lcec_dout_write(slave, pin);
+  }
+}
--- src/devices/lcec_el2xxx.c
+++ src/devices/lcec_el2xxx.c
@@ -16,99 +16,68 @@
 //    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 //
 
-#include "../lcec.h"
 #include "lcec_el2xxx.h"
+
+#include "../lcec.h"
+#include "lcec_class_dout.h"
 
 static int lcec_el2xxx_init(int comp_id, struct lcec_slave *slave, ec_pdo_entry_reg_t *pdo_entry_regs);
 
-static lcec_typelist_t types[]={
-  { "EL2002", LCEC_BECKHOFF_VID, 0x07D23052, LCEC_EL2002_PDOS, 0, NULL, lcec_el2xxx_init},
-  { "EL2004", LCEC_BECKHOFF_VID, 0x07D43052, LCEC_EL2004_PDOS, 0, NULL, lcec_el2xxx_init},
-  { "EL2008", LCEC_BECKHOFF_VID, 0x07D83052, LCEC_EL2008_PDOS, 0, NULL, lcec_el2xxx_init},
-  { "EL2022", LCEC_BECKHOFF_VID, 0x07E63052, LCEC_EL2022_PDOS, 0, NULL, lcec_el2xxx_init},
-  { "EL2024", LCEC_BECKHOFF_VID, 0x07E83052, LCEC_EL2024_PDOS, 0, NULL, lcec_el2xxx_init},
-  { "EL2032", LCEC_BECKHOFF_VID, 0x07F03052, LCEC_EL2032_PDOS, 0, NULL, lcec_el2xxx_init},
-  { "EL2034", LCEC_BECKHOFF_VID, 0x07F23052, LCEC_EL2034_PDOS, 0, NULL, lcec_el2xxx_init},
-  { "EL2042", LCEC_BECKHOFF_VID, 0x07FA3052, LCEC_EL2042_PDOS, 0, NULL, lcec_el2xxx_init},
-  { "EL2084", LCEC_BECKHOFF_VID, 0x08243052, LCEC_EL2084_PDOS, 0, NULL, lcec_el2xxx_init},
-  { "EL2088", LCEC_BECKHOFF_VID, 0x08283052, LCEC_EL2088_PDOS, 0, NULL, lcec_el2xxx_init},
-  { "EL2124", LCEC_BECKHOFF_VID, 0x084C3052, LCEC_EL2124_PDOS, 0, NULL, lcec_el2xxx_init},
-  { "EL2612", LCEC_BECKHOFF_VID, 0x0A343052, LCEC_EL2612_PDOS, 0, NULL, lcec_el2xxx_init},
-  { "EL2622", LCEC_BECKHOFF_VID, 0x0A3E3052, LCEC_EL2622_PDOS, 0, NULL, lcec_el2xxx_init},
-  { "EL2634", LCEC_BECKHOFF_VID, 0x0A4A3052, LCEC_EL2634_PDOS, 0, NULL, lcec_el2xxx_init},
-  { "EL2652", LCEC_BECKHOFF_VID, 0x0A5C3052, LCEC_EL2652_PDOS, 0, NULL, lcec_el2xxx_init},
-  { "EL2808", LCEC_BECKHOFF_VID, 0x0AF83052, LCEC_EL2808_PDOS, 0, NULL, lcec_el2xxx_init},
-  { "EL2798", LCEC_BECKHOFF_VID, 0x0AEE3052, LCEC_EL2798_PDOS, 0, NULL, lcec_el2xxx_init},
-  { "EL2809", LCEC_BECKHOFF_VID, 0x0AF93052, LCEC_EL2809_PDOS, 0, NULL, lcec_el2xxx_init},
-  { "EP2008", LCEC_BECKHOFF_VID, 0x07D84052, LCEC_EP2008_PDOS, 0, NULL, lcec_el2xxx_init},
-  { "EP2028", LCEC_BECKHOFF_VID, 0x07EC4052, LCEC_EP2028_PDOS, 0, NULL, lcec_el2xxx_init},
-  { "EP2809", LCEC_BECKHOFF_VID, 0x0AF94052, LCEC_EP2809_PDOS, 0, NULL, lcec_el2xxx_init},
-  { NULL },
+static lcec_typelist_t types[] = {
+    {"EL2002", LCEC_BECKHOFF_VID, 0x07D23052, LCEC_EL2002_PDOS, 0, NULL, lcec_el2xxx_init},
+    {"EL2004", LCEC_BECKHOFF_VID, 0x07D43052, LCEC_EL2004_PDOS, 0, NULL, lcec_el2xxx_init},
+    {"EL2008", LCEC_BECKHOFF_VID, 0x07D83052, LCEC_EL2008_PDOS, 0, NULL, lcec_el2xxx_init},
+    {"EL2022", LCEC_BECKHOFF_VID, 0x07E63052, LCEC_EL2022_PDOS, 0, NULL, lcec_el2xxx_init},
+    {"EL2024", LCEC_BECKHOFF_VID, 0x07E83052, LCEC_EL2024_PDOS, 0, NULL, lcec_el2xxx_init},
+    {"EL2032", LCEC_BECKHOFF_VID, 0x07F03052, LCEC_EL2032_PDOS, 0, NULL, lcec_el2xxx_init},
+    {"EL2034", LCEC_BECKHOFF_VID, 0x07F23052, LCEC_EL2034_PDOS, 0, NULL, lcec_el2xxx_init},
+    {"EL2042", LCEC_BECKHOFF_VID, 0x07FA3052, LCEC_EL2042_PDOS, 0, NULL, lcec_el2xxx_init},
+    {"EL2084", LCEC_BECKHOFF_VID, 0x08243052, LCEC_EL2084_PDOS, 0, NULL, lcec_el2xxx_init},
+    {"EL2088", LCEC_BECKHOFF_VID, 0x08283052, LCEC_EL2088_PDOS, 0, NULL, lcec_el2xxx_init},
+    {"EL2124", LCEC_BECKHOFF_VID, 0x084C3052, LCEC_EL2124_PDOS, 0, NULL, lcec_el2xxx_init},
+    {"EL2612", LCEC_BECKHOFF_VID, 0x0A343052, LCEC_EL2612_PDOS, 0, NULL, lcec_el2xxx_init},
+    {"EL2622", LCEC_BECKHOFF_VID, 0x0A3E3052, LCEC_EL2622_PDOS, 0, NULL, lcec_el2xxx_init},
+    {"EL2634", LCEC_BECKHOFF_VID, 0x0A4A3052, LCEC_EL2634_PDOS, 0, NULL, lcec_el2xxx_init},
+    {"EL2652", LCEC_BECKHOFF_VID, 0x0A5C3052, LCEC_EL2652_PDOS, 0, NULL, lcec_el2xxx_init},
+    {"EL2808", LCEC_BECKHOFF_VID, 0x0AF83052, LCEC_EL2808_PDOS, 0, NULL, lcec_el2xxx_init},
+    {"EL2798", LCEC_BECKHOFF_VID, 0x0AEE3052, LCEC_EL2798_PDOS, 0, NULL, lcec_el2xxx_init},
+    {"EL2809", LCEC_BECKHOFF_VID, 0x0AF93052, LCEC_EL2809_PDOS, 0, NULL, lcec_el2xxx_init},
+    {"EP2008", LCEC_BECKHOFF_VID, 0x07D84052, LCEC_EP2008_PDOS, 0, NULL, lcec_el2xxx_init},
+    {"EP2028", LCEC_BECKHOFF_VID, 0x07EC4052, LCEC_EP2028_PDOS, 0, NULL, lcec_el2xxx_init},
+    {"EP2809", LCEC_BECKHOFF_VID, 0x0AF94052, LCEC_EP2809_PDOS, 0, NULL, lcec_el2xxx_init},
+    {NULL},
 };
 ADD_TYPES(types);
-
-typedef struct {
-  hal_bit_t *out;
-  hal_bit_t invert;
-  unsigned int pdo_os;
-  unsigned int pdo_bp;
-} lcec_el2xxx_pin_t;
 
 static void lcec_el2xxx_write(struct lcec_slave *slave, long period);
 
 static int lcec_el2xxx_init(int comp_id, struct lcec_slave *slave, ec_pdo_entry_reg_t *pdo_entry_regs) {
   lcec_master_t *master = slave->master;
-  lcec_el2xxx_pin_t *hal_data;
-  lcec_el2xxx_pin_t *pin;
+  lcec_class_dout_pins_t *hal_data;
   int i;
-  int err;
 
   // initialize callbacks
   slave->proc_write = lcec_el2xxx_write;
 
-  // alloc hal memory
-  if ((hal_data = hal_malloc(sizeof(lcec_el2xxx_pin_t) * slave->pdo_entry_count)) == NULL) {
-    rtapi_print_msg(RTAPI_MSG_ERR, LCEC_MSG_PFX "hal_malloc() for slave %s.%s failed\n", master->name, slave->name);
+  hal_data = lcec_dout_allocate_pins(slave->pdo_entry_count);
+  if (hal_data == NULL) {
     return -EIO;
   }
-  memset(hal_data, 0, sizeof(lcec_el2xxx_pin_t) * slave->pdo_entry_count);
   slave->hal_data = hal_data;
 
   // initialize pins
-  for (i=0, pin=hal_data; i<slave->pdo_entry_count; i++, pin++) {
-    // initialize POD entry
-    LCEC_PDO_INIT(pdo_entry_regs, slave->index, slave->vid, slave->pid, 0x7000 + (i << 4), 0x01, &pin->pdo_os, &pin->pdo_bp);
-
-    // export pins
-    if ((err = lcec_pin_newf(HAL_BIT, HAL_IN, (void **) &(pin->out), "%s.%s.%s.dout-%d", LCEC_MODULE_NAME, master->name, slave->name, i)) != 0) {
-      return err;
-    }
-    if ((err = lcec_param_newf(HAL_BIT, HAL_RW, (void *) &(pin->invert), "%s.%s.%s.dout-%d-invert", LCEC_MODULE_NAME, master->name, slave->name, i)) != 0) {
-      return err;
-    }
-
-    // initialize pins
-    *(pin->out) = 0;
-    pin->invert = 0;
+  for (i = 0; i < slave->pdo_entry_count; i++) {
+    hal_data->pins[i] = lcec_dout_register_pin(&pdo_entry_regs, slave, i, 0x7000 + (i << 4), 0x01);
   }
 
   return 0;
 }
 
 static void lcec_el2xxx_write(struct lcec_slave *slave, long period) {
-  lcec_master_t *master = slave->master;
-  lcec_el2xxx_pin_t *hal_data = (lcec_el2xxx_pin_t *) slave->hal_data;
-  uint8_t *pd = master->process_data;
-  lcec_el2xxx_pin_t *pin;
-  int i, s;
+  lcec_class_dout_pins_t *hal_data = (lcec_class_dout_pins_t *)slave->hal_data;
 
-  // set outputs
-  for (i=0, pin=hal_data; i<slave->pdo_entry_count; i++, pin++) {
-    s = *(pin->out);
-    if (pin->invert) {
-      s = !s;
-    }
-    EC_WRITE_BIT(&pd[pin->pdo_os], pin->pdo_bp, s);
+  if (!slave->state.operational) {
+    return;
   }
+  lcec_dout_write_all(slave, hal_data);
 }
-
